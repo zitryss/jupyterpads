9ee5b9fc66f436897641dca84cdb2a3b77b572a2
Eugene Russkikh <Evgenii.Russkikh@msg.group>
2021-01-17 19:09:06 +0100

    Fix update bug

--- src/components/output.tsx
+++ src/components/output.tsx
@@ -44,7 +44,7 @@ export default function Ouput(props: IProps) {
   const [df, setDf] = useState(defaultDataFrame);
 
   useEffect(() => {
-    if (!isFirstRun) {
+    if (isFirstRun) {
       update();
       setIsFirstRun(false);
     }
